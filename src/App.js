294486ee7573c9783454f2187925506f1079f534
culleton <ian.culleton.92@gmail.com>
2023-03-13 12:05:00 -0400

    useState refactor

--- src/App.js
+++ src/App.js
@@ -1,27 +1,26 @@
 import './App.css';
-import React from 'react';
+import React, { useState } from 'react';
 import NewToDo from './components/NewToDo';
 import ToDoList from './components/ToDoList';
 
 function App() {
-  let todos = [
+  let initialTodos = [
     { name: 'laundry' },
     { name: 'buy groceries' },
     { name: 'mow lawn' }
   ]
 
-  let currentItem = {
-    name: ''
-  }
+  const [todos, setTodos] = useState(initialTodos);
+  const [currentItem, setCurrentItem] = useState({name: ''});
 
   const handleChange = (e) => {
-    currentItem =  { name: e.target.value }
+    setCurrentItem({ name: e.target.value });
   }
 
   const handleSubmit = (e) => {
     e.preventDefault()
-    todos.push(currentItem);
-    currentItem = { name: '' }
+    setTodos([...todos, currentItem]);
+    setCurrentItem({ name: '' });
   }
 
   return (
