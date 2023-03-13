f5a36cd3d54eda41f6a3be1031e3208ba50c52a6
culleton <ian.culleton.92@gmail.com>
2023-03-13 12:53:05 -0400

    useReducer refactor

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import React, { useState } from 'react';
+import React, { useState, useReducer } from 'react';
 import NewToDo from './components/NewToDo';
 import ToDoList from './components/ToDoList';
 
@@ -10,21 +10,46 @@ function App() {
     { name: 'mow lawn' }
   ]
 
-  const [todos, setTodos] = useState(initialTodos);
-  const [currentItem, setCurrentItem] = useState({name: ''});
+  const todosReducer = (prevTodos, action) => {
+    switch (action.type) {
+      case "ADD_TODO":
+        return [...prevTodos, action.payload]
+      case "RESET_TODOS":
+        return []
+      default: 
+        return prevTodos;
+    }
+  }
+
+  const currentItemReducer = (prevCurrentItemState, action) => {
+    switch (action.type) {
+      case "SET_CURRENT_ITEM":
+        return {name: action.payload}
+
+    }
+  }
+
+  const [todos, todosDispatch] = useReducer(todosReducer, initialTodos);
+  const [currentItem, currentItemDispatch] = useReducer(currentItemReducer, {name: ''});
+
 
   const handleChange = (e) => {
-    setCurrentItem({ name: e.target.value });
+    currentItemDispatch({ type: "SET_CURRENT_ITEM", payload: e.target.value });
   }
 
   const handleSubmit = (e) => {
     e.preventDefault()
-    setTodos([...todos, currentItem]);
-    setCurrentItem({ name: '' });
+    const action = { type: "ADD_TODO", payload: currentItem }
+    todosDispatch(action);
+  }
+
+  const resetTodos = () => {
+    todosDispatch({ type: "RESET_TODOS" })
   }
 
   return (
       <div className="App">
+        <button onClick={resetTodos}>Reset Todos</button>
         <ToDoList todos={todos} />
         <NewToDo onChange={handleChange} onSubmit={handleSubmit} />
       </div>
